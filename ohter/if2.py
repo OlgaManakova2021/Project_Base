c32700d2b44329b75993d4328afa9f72122b75e2
Olga <manakovaolga2012@yandex.ru>
2021-07-12 19:58:00 +0300

    забрать

--- ohter/if2.py
+++ ohter/if2.py
@@ -1,7 +1,7 @@
 # программа калькулятор
 
 Mum1 = int(input('Введи 1 число: '))
-Mum2 = int(input('Введи второе число: '))
+Mum2 = int(input('Введи 2 число: '))
 calc = input('Введи знак операции:  ')
 if calc == '+':
     print(Mum1 + Mum2)
