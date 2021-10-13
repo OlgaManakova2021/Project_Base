ed2765bfa3f49e5daf6d1f13c639feb2c50d6278
OLGA <manakovaolga2012@yandex.ru>
2021-10-13 16:46:39 +0300

    commit new task

--- ohter/While_prog.py
+++ ohter/While_prog.py
@@ -20,8 +20,3 @@ print()
 print('Программа успешно завершена!')
 
 
-# while True:
-#     name = input('Enter name: ')
-#     if name == 'stop': break
-#     age = input ('Enter age: ')
-#     print('Hello', name, '=>', int(age) ** 2)
