a68ec8d976b5bb8afe8a605fc20f57c26199532a
olgamanakova@yandex.ru <manakovaolga2012@yandex.ru>
2022-03-04 22:55:27 +0300

    к теме Модули

--- def_exp/def_2.py
+++ def_exp/def_2.py
@@ -1,14 +1,23 @@
 def rectangle():
+    """Вычисление площади прямоугольника"""
     a = float(input("Ширина %s: " % figure)) # обращение к глобальной
     b = float(input("Высота %s: " % figure)) # переменной figure
-    print("Площадь: %.2f" % (a*b))
+    return print("Площадь: %.2f" % (a*b))
+
 def triangle():
+    """Вычисление площади треугольника
+
+    Используется общепринятая формула
+
+    """
     a = float(input("Основание %s: " % figure))
     h = float(input("Высота %s: " % figure))
-    print("Площадь: %.2f" % (0.5 * a * h))
+    return print("Площадь: %.2f" % (0.5 * a * h))
+
 figure = input("1-прямоугольник, 2-треугольник: ")
 if figure == '1':
     rectangle()
 elif figure == '2':
     triangle()
 
+print(rectangle.__doc__)
