d915d28a3a10cfcf780c4a744039522c9875938a
olgamanakova@yandex.ru <manakovaolga2012@yandex.ru>
2022-03-09 10:20:18 +0300

    тема Пакеты

---
+++ module/__init__.py
@@ -0,0 +1,9 @@
+import module.lessons_set
+from .lessons_set import *
+from .lessons_file import *
+from .doc import *
+
+NAME = "курс лекций по Python"
+
+
+#from . import lessons_set, lessons_dict, lessons_file, lessons_list
