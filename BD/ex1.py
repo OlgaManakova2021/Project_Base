978454271557a806f990409578eb1e7791156e33
olgamanakova@yandex.ru <manakovaolga2012@yandex.ru>
2022-04-01 21:47:31 +0300

    БД, SQL, INSERT

--- BD/ex1.py
+++ BD/ex1.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+from date.data_users import info_users
 
 with sq.connect('saper.db') as con:
     cur = con.cursor()
@@ -10,4 +11,17 @@ with sq.connect('saper.db') as con:
         old INTEGER,
         score INTEGER
     )""")
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+
+    # cur.execute("INSERT INTO users VALUES (1, 'Алексей', 1, 22, 1000)")
+    # cur.execute("INSERT INTO users VALUES (2, 'Миша', 1, 19, 800)")
+    # cur.execute("INSERT INTO users VALUES (3, 'Федор', 1, 26, 1100)")
+    # cur.execute("INSERT INTO users VALUES (4, 'Маша', 2, 18, 1500)")
+
+
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+
+
 
