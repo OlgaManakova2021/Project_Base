2696d38f772eb17e21df969a2615fe0342390438
olgamanakova@yandex.ru <manakovaolga2012@yandex.ru>
2022-04-04 09:55:40 +0300

    select

--- BD/ex1.py
+++ BD/ex1.py
@@ -4,13 +4,13 @@ from date.data_users import info_users
 with sq.connect('saper.db') as con:
     cur = con.cursor()
     #cur.execute("DROP TABLE IF EXISTS users")
-    cur.execute("""CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        sex INTEGER NOT NULL DEFAULT 1,
-        old INTEGER,
-        score INTEGER
-    )""")
+    # cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    #     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     name TEXT NOT NULL,
+    #     sex INTEGER NOT NULL DEFAULT 1,
+    #     old INTEGER,
+    #     score INTEGER
+    # )""")
 
 with sq.connect('saper.db') as con:
     cur = con.cursor()
@@ -21,7 +21,26 @@ with sq.connect('saper.db') as con:
     # cur.execute("INSERT INTO users VALUES (4, 'Маша', 2, 18, 1500)")
 
 
-    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+    #cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+
+    # cur.execute("SELECT * FROM users WHERE score > 1000 ORDER BY score DESC")
+    # result = cur.fetchall()
+    # print(result)
+
+    cur.execute("SELECT * FROM users WHERE score > 1000 ORDER BY score DESC")
+    for result in cur:
+        print(result)
+
+    cur.execute("SELECT * FROM users")
+    result1 = cur.fetchone()
+    result2 = cur.fetchmany(2)
+    print(result1)
+    print(result2)
+
+
+
+
+
 
 
 
