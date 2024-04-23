ad70531695fbcf3c4599133a0f0212212b4cfaa3
olgamanakova@yandex.ru <manakvaolga2012@yandex.ru>
2024-04-23 10:45:25 +0300

    update Part2.py

--- OOP/Part2.py
+++ OOP/Part2.py
@@ -7,13 +7,15 @@ class Note:
         self.count += 1
 
 note1 = Note("Задача 1")
-print(note1.__dict__)
-print(dir(note1))
-print(note1.text)
+# print(note1.__dict__)
+# print(dir(note1))
+# print(note1.text)
+#
+# note1.upcount()
+# print(note1.count)
+# note1.upcount()
+# print(note1.count)
 
-note1.upcount()
-print(note1.count)
-note1.upcount()
-print(note1.count)
-
-
+# Привязанные методы
+print(note1.upcount) # bound - метод является привязанным для экземпляра note1
+print(Note.upcount) # function -  с точки зрения класса Note upcount является обычной функцией
